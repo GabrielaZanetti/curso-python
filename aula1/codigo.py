0cc59f0d3046a590e258f7b65e1a659ff22983d6
GabrielaZanetti <gabiisctt@gmail.com>
2024-07-09 16:25:11 -0300

    loop dos dados da tabela capturando a coluna e armazendando em variavel

--- aula1/codigo.py
+++ aula1/codigo.py
@@ -32,3 +32,40 @@ pyautogui.click(x=-698, y=536) # click no botao de login
 
 produtos = os.path.join(os.path.dirname(__file__),"produtos.csv") # encontra o arquivo
 dados_produto = pandas.read_csv(produtos)
+
+for linha in dados_produto.index:
+    pyautogui.click(x=-966, y=265) # click no input do codigo 
+
+    codigo = str(dados_produto.loc[linha, "codigo"])
+    pyautogui.write(codigo)
+    pyautogui.hotkey("tab")
+
+    marca = str(dados_produto.loc[linha, "marca"])
+    pyautogui.write(marca)
+    pyautogui.hotkey("tab")
+
+    tipo = str(dados_produto.loc[linha, "tipo"])
+    pyautogui.write(tipo)
+    pyautogui.hotkey("tab")
+
+    categoria = str(dados_produto.loc[linha, "categoria"])
+    pyautogui.write(categoria)
+    pyautogui.hotkey("tab")
+
+    preco_unitario = str(dados_produto.loc[linha, "preco_unitario"])
+    pyautogui.write(preco_unitario)
+    pyautogui.hotkey("tab")
+
+    custo = str(dados_produto.loc[linha, "custo"])
+    pyautogui.write(custo)
+
+    pyautogui.hotkey("tab")
+
+    obs = str(dados_produto.loc[linha, "obs"])
+    if obs != "nan":
+        pyautogui.write(obs)
+
+    pyautogui.hotkey("tab") # botao de enviar
+
+    pyautogui.hotkey("enter")
+    pyautogui.scroll(1000) # voltar ao inicio da tela
