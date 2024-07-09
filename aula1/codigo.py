35820979a4cf8961f88236b7b6bfde95b215c66a
GabrielaZanetti <gabiisctt@gmailcom>
2024-07-09 14:27:11 -0300

    passo a passo da atividade a ser feita

---
+++ aula1/codigo.py
@@ -0,0 +1,6 @@
+# Passo 1: Acessar o site da empresa
+#    Link: https://dlp.hashtagtreinamentos.com/python/intensivao/login
+# Passo 2: Fazer Login
+# Passo 3: Pegar/importar a base de dados
+# Passo 4: Cadastrar o primeiro produto
+# Passo 5: Repetir o passo 4 até cadastrar todos os produtos
