13c680679334b984e4345b2cd7967bd635895995
GabrielaZanetti <gabiisctt@gmail.com>
2024-07-09 14:54:12 -0300

    abrir o site usando o pyautogui para automatizar o teclado

--- aula1/codigo.py
+++ aula1/codigo.py
@@ -4,3 +4,14 @@
 # Passo 3: Pegar/importar a base de dados
 # Passo 4: Cadastrar o primeiro produto
 # Passo 5: Repetir o passo 4 até cadastrar todos os produtos
+
+import pyautogui # automatiza mouse/teclado/tela
+
+pyautogui.PAUSE = 0.5 # a cada comando do pyautogui faz uma pausa de meio segundo
+
+pyautogui.press("win")
+pyautogui.write("Crome")
+pyautogui.press("enter")
+
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press("enter")
