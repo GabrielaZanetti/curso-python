19d347c8f5282ba870ed35dbe1abc29964192d63
GabrielaZanetti <gabiisctt@gmail.com>
2024-07-15 15:17:20 -0300

    mensagem de acesso ao chat e envio de mensagem no chat

--- hashzap.py
+++ hashzap.py
@@ -16,14 +16,28 @@ def main(pagina): # função principal
     campo_nome_usuario = ft.TextField(label="Escreva seu nome no chat")
 
     mensagem_texto = ft.TextField(label="Digite sua mensagem")
-    btn_enviar = ft.ElevatedButton("Enviar")
+
+    def enviar_mensagem(event):
+        mensagem_user = f"{campo_nome_usuario.value}: {mensagem_texto.value}"
+        chat.controls.append(ft.Text(mensagem_user))
+        
+        mensagem_texto.value = ""
+        pagina.update() # atualiza a view da tela
+
+    btn_enviar = ft.ElevatedButton("Enviar", on_click=enviar_mensagem)
+
+    chat = ft.Column()
+    linha_mensagem = ft.Row([ mensagem_texto, btn_enviar ])
 
     def entrar_chat(event):
         # remover o titulo, remover o btn entrar, fechar janela, criar/campo texto/botão enviar do chat
         pagina.remove(titulo, botao_iniciar)
         janela.open = False
 
-        pagina.add(mensagem_texto, btn_enviar)
+        pagina.add(chat, linha_mensagem)
+        
+        text_entrou_chat = f"{campo_nome_usuario.value} entrou no chat"
+        chat.controls.append(ft.Text(text_entrou_chat))
         pagina.update() # atualiza a view da tela
 
     botao_entrar = ft.ElevatedButton("Entrar no Chat", on_click=entrar_chat)
